c075057f7e773b2065dbf4cefc069770a1028ef4
11249a252-ops <11249a252@kanchiuniv.ac.in>
2026-04-23 15:13:43 +0530

    Add R programming basics with example scenarios
    Added various examples demonstrating basic R programming concepts, including data types and variable assignments for different scenarios.

---
+++ 1. Learn all the Basics of R-Programming (Data types, Variables, Operators etc..).r
@@ -0,0 +1,81 @@
+# Experiment No: 1
+
+# Question 1: Student Information System
+roll_no <- 202
+name <- "Ravi"
+cgpa <- 7.9
+pass <- TRUE
+roll_no
+name
+cgpa
+pass
+
+# Question 2: Employee Salary Record
+emp_id <- 6002
+salary <- 52000.50
+department <- "HR"
+is_permanent <- FALSE
+emp_id
+salary
+department
+is_permanent
+
+# Question 3: Online Shopping Cart
+product_name <- "Mobile"
+price <- 30000
+stock_available <- 15
+discount_eligible <- TRUE
+product_name
+price
+stock_available
+discount_eligible
+
+# Question 4: Weather Monitoring System
+city <- "Mumbai"
+temperature <- 33.2
+rain <- TRUE
+wind_speed <- 10.5
+city
+temperature
+rain
+wind_speed
+
+# Question 5: Examination Result Processing
+subject <- "Physics"
+total_marks <- 100
+marks_obtained <- 65
+result <- marks_obtained >= 40
+subject
+total_marks
+marks_obtained
+result
+
+# Question 6: Banking Application
+account_no <- 987654321
+holder_name <- "Suresh"
+balance <- 15000.75
+account_active <- TRUE
+account_no
+holder_name
+balance
+account_active
+
+# Question 7: Movie Rating System
+movie_title <- "Avatar"
+rating <- 9.1
+reviews <- 2000
+available <- FALSE
+movie_title
+rating
+reviews
+available
+
+# Question 8: Traffic Monitoring System
+vehicle_count <- 220
+location <- "City Center"
+avg_speed <- 30.2
+congested <- TRUE
+vehicle_count
+location
+avg_speed
+congested
